8a1686206bbeb759a5fb677c13939f32e2eb24ef
ASUKA <asuka@henchat.net>
2026-06-30 23:39:55 +0900

    Added kernal compiler

--- dev/install-devtoolset-11.sh
+++ dev/install-devtoolset-11.sh
@@ -1,3 +1,4 @@
 yum install -y devtoolset-11
-scl enable devtoolset-11 bash
-echo "source /opt/rh/devtoolset-11/enable" >> ~/.bashrc
+# scl enable devtoolset-11 bash
+echo 'source /opt/rh/devtoolset-11/enable' | sudo tee /etc/profile.d/devtoolset.sh
+/opt/rh/devtoolset-11/enable

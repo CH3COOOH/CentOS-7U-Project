c21c23ea3ee4ca2826b9300d6529c7f4c15faa13
ASUKA <asuka@henchat.net>
2026-07-05 00:13:56 +0900

    commit-20260705

--- dev/compile-kernel-6.6.sh
+++ dev/compile-kernel-6.6.sh
@@ -10,21 +10,22 @@ yum install -y ncurses-devel openssl-devel elfutils-libelf-devel bc flex bison
 
 echo "[2/5] Downloading kernel source codes..."
 cd /usr/src
-# curl -LO https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KVER.tar.xz
+curl -LO https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KVER.tar.xz
 tar -xf linux-$KVER.tar.xz
 cd linux-$KVER
 
 echo "[3/5] Generating configure..."
 cp -v /boot/config-$(uname -r) .config || true
 yes "" | make oldconfig
+make menuconfig
 
 echo "[4/5] Compiling kernal and modules..."
 make -j"$JOBS"
 make -j"$JOBS" modules
 
-# echo "[5/5] Installing kernal..."
-# make modules_install
-# make install
-# grub2-mkconfig -o /boot/grub2/grub.cfg
+echo "[5/5] Installing kernal..."
+make modules_install
+make install
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
-# echo "Done. Please reboot."
+echo "Done."
